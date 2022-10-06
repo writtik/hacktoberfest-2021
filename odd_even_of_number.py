95a5b3cae7c0eb41dd5582b406b572599d100ad4
writtikNath <92996755+writtikNath@users.noreply.github.com>
2022-10-06 20:15:17 +0530

    Update odd_even_of_number.py
    Dict changes

--- odd_even_of_number.py
+++ odd_even_of_number.py
@@ -3,6 +3,7 @@
 # If the remainder is 1, it is an odd number.
 
 num_list = input("Enter a list of number: "))
+num_dict = input("Enter a dict of count and list number: "))
 # find odd even in array
 # check if only int instance
 for num in num_list:
