04c532e15175bd3f47bb2977350f2307b1533920
writtikNath <92996755+writtikNath@users.noreply.github.com>
2022-10-06 20:04:22 +0530

    Update odd_even_of_number.py
    List updation

--- odd_even_of_number.py
+++ odd_even_of_number.py
@@ -2,11 +2,13 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("WE ARE HERE in IF")
-   print("{0} is Even".format(num))
-else:
-   # for debug
-   print("WE ARE HERE")
-   print("{0} is Odd".format(num))
+num_list = input("Enter a list of number: "))
+# find odd even in array
+for num in num_list:
+   if (num % 2) == 0:
+      print("WE ARE HERE in IF")
+      print("{0} is Even".format(num))
+   else:
+      # for debug
+      print("WE ARE HERE")
+      print("{0} is Odd".format(num))
