5cc5f55de637199cd12712655e425b650cf33d62
writtikNath <92996755+writtikNath@users.noreply.github.com>
2022-10-06 20:10:21 +0530

    Update odd_even_of_number.py
    instance added

--- odd_even_of_number.py
+++ odd_even_of_number.py
@@ -4,11 +4,13 @@
 
 num_list = input("Enter a list of number: "))
 # find odd even in array
+# check if only int instance
 for num in num_list:
-   if (num % 2) == 0:
-      print("WE ARE HERE in IF")
-      print("{0} is Even".format(num))
-   else:
-      # for debug
-      print("WE ARE HERE")
-      print("{0} is Odd".format(num))
+   if isinstance(int, num):
+      if (num % 2) == 0:
+         print("WE ARE HERE in IF")
+         print("{0} is Even".format(num))
+      else:
+         # for debug
+         print("WE ARE HERE")
+         print("{0} is Odd".format(num))
